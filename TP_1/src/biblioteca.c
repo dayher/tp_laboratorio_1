498fe4fda29f9f3f03b927346f5d4cd683626572
Dayher <dayhermarchan@gmail.com>
2021-09-03 00:07:53 -0300

    Initial commit

---
+++ TP_1/src/biblioteca.c
@@ -0,0 +1,47 @@
+/*
+ * biblioteca.c
+ *
+ *  Created on: 1 sep. 2021
+ *      Author: dayher
+ */
+
+#include "biblioteca.h"
+
+
+
+double sumar(double primero, double segundo){
+	double resultado;
+	resultado = primero + segundo;
+	return resultado;
+}
+
+double restar(double primero, double segundo){
+	double resultado;
+	resultado = primero - segundo;
+	return resultado;
+}
+
+double multiplicar(double primero, double segundo){
+	double resultado;
+	resultado = primero * segundo;
+	return resultado;
+}
+double dividir(double primero, double segundo){
+	double resultado;
+	resultado = primero / segundo;
+	return resultado;
+}
+long factorial(unsigned numero){
+
+	long resultado=1;
+	unsigned contador=0;
+
+	if(numero==0 || numero==1){
+		return resultado;
+	} else {
+		for(contador=numero;contador>1;contador--){
+			resultado*=contador;
+		}
+		return resultado;
+	}
+}
---
+++ TP_1/src/biblioteca.h
@@ -0,0 +1,19 @@
+/*
+ * biblioteca.h
+ *
+ *  Created on: 1 sep. 2021
+ *      Author: dayher
+ */
+
+#ifndef BIBLIOTECA_H_
+#define BIBLIOTECA_H_
+
+
+
+#endif /* BIBLIOTECA_H_ */
+
+double sumar(double a, double b);
+double restar(double a, double b);
+double multiplicar(double a, double b);
+double dividir(double a, double b);
+long factorial(unsigned n);
